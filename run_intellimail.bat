98e242784d2851081b715615534e8337da0512b4
A-AZALMAT <115454892+A-AZALMAT@users.noreply.github.com>
2025-04-14 12:03:13 +0100

    Update run_intellimail.bat

--- run_intellimail.bat
+++ run_intellimail.bat
@@ -1,2 +1,3 @@
 SET PYEXOSEU_ENV_NAME=intellimail_env
-call micromamba run -n %PYEXOSEU_ENV_NAME% streamlit main.py
+cd /d C:\Users\%USERNAME%\intellimail
+call micromamba run -n %PYEXOSEU_ENV_NAME% streamlit run main.py
